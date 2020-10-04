0159ae2482764b13c109c1db05a0b7f9eaf0354c
EC2 Default User <ec2-user@ip-172-31-85-211.ec2.internal>
2020-10-04 05:37:44 +0000

    [finish]bookers2-debug

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+  attachment :profile_image, destroy: false
+  has_many :books, dependent: :destroy
+
+  validates :name, presence: true, length: {maximum: 20, minimum: 2}, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+
+  def posts
+  return Book.where(user_id: self.id)
+  end
+
+end
