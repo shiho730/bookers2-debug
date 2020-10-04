e52f9dd8cd9a7972628d1cc9e67a5a36f24622a9
EC2 Default User <ec2-user@ip-172-31-85-211.ec2.internal>
2020-10-04 15:07:46 +0000

    [fix]likeandcomments

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,7 +21,7 @@ class BooksController < ApplicationController
     @book_new = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
-    @post_comment = PostComment.new
+    @book_comment = BookComment.new
   end
 
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   attachment :profile_image, destroy: false
   has_many :books, dependent: :destroy
-  has_many :post_comments, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
   validates :name, presence: true, length: {maximum: 20, minimum: 2}, uniqueness: true
